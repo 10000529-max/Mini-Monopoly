0f2e7d19f658d32ca5f8353fd5822f5e14d49a71
Ghassan T <10000529@philasd.org>
2026-09-11 18:33:56 +0000

    step 2

--- Vexcode_monopoly.py
+++ Vexcode_monopoly.py
@@ -9,15 +9,75 @@ dice2 = 0
 spaces_to_move = 0
 current_space_number = 0
 
-def roll_dice():
+def complete_task():
     global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    dice1 = int(round(urandom.uniform(1, 4), 2))
-    brain.screen.print(str("Rolled a:") + str(dice1))
+    brain.screen.print(str("LandedSpace") + str(current_space_number))
     brain.screen.next_row()
+    wait(1, SECONDS)
+    if current_space_number == 1:
+        # Space = GO
+        for repeat_count in range(4):
+            Left.broadcast_and_wait()
+            wait(5, MSEC)
+    elif current_space_number == 4:
+        # Space = Jail
+        wait(3, SECONDS)
+    elif current_space_number == 7:
+        # Space = Free parking
+        wait(5, SECONDS)
+    elif current_space_number == 10:
+        # Space = Go to Jail
+        Right.broadcast_and_wait()
+        for repeat_count2 in range(3):
+            Foward.broadcast_and_wait()
+            wait(5, MSEC)
+        Left.broadcast_and_wait()
+        for repeat_count3 in range(3):
+            Foward.broadcast_and_wait()
+            wait(5, MSEC)
+        for repeat_count4 in range(2):
+            Right.broadcast_and_wait()
+            wait(5, MSEC)
+        current_space_number = 4
+        wait(1, SECONDS)
+    else:
+        # Space = Blue 1 or 2, Green 1 or 2, Yellow 1 or 2, or Red 1 or 2
+        Right.broadcast_and_wait()
+        Foward.broadcast_and_wait()
+        for repeat_count5 in range(2):
+            Left.broadcast_and_wait()
+            wait(5, MSEC)
+        Foward.broadcast_and_wait()
+        Right.broadcast_and_wait()
 
 def move():
     global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
+    brain.screen.print(str("moving") + str(str(spaces_to_move) + str("spaces")))
+    brain.screen.next_row()
+    for repeat_count6 in range(int(spaces_to_move)):
+        Foward.broadcast_and_wait()
+        current_space_number = current_space_number + 1
+        if current_space_number > 12:
+            current_space_number = 1
+        if current_space_number == 1:
+            Right.broadcast_and_wait()
+        if current_space_number == 4:
+            Right.broadcast_and_wait()
+        if current_space_number == 7:
+            Right.broadcast_and_wait()
+        if current_space_number == 10:
+            Right.broadcast_and_wait()
+        wait(5, MSEC)
+
+def roll_dice():
+    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
+    dice1 = int(round(urandom.uniform(1, 5), 2))
+    brain.screen.print(str("Rolled a:") + str(dice1))
+    brain.screen.next_row()
+    dice2 = int(round(urandom.uniform(1, 4), 2))
+    brain.screen.print(str("Rolled a:") + str(dice2))
+    brain.screen.next_row()
+    spaces_to_move = dice1 + dice2
 
 def play_game():
     global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
@@ -25,54 +85,8 @@ def play_game():
         roll_dice()
         move()
         complete_task()
+        wait(3, SECONDS)
+        brain.screen.set_cursor(1, 1)
+        brain.screen.clear_row(4)
+        brain.screen.set_cursor(brain.screen.row(), 1)
         wait(5, MSEC)
-
-def complete_task():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def Foward_callback_0():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def when_started1():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    current_space_number = 1
-    play_game()
-
-def Left_callback_0():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def Right_callback_0():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def when_started2():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def Foward_callback_1():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def Left_callback_1():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-def Right_callback_1():
-    global FWD, LT, RT, my_event, Foward, Left, Right, myVariable, dice1, dice2, spaces_to_move, current_space_number, screen_precision, console_precision
-    pass
-
-# system event handlers
-Foward(Foward_callback_0)
-Foward(Foward_callback_1)
-Left(Left_callback_0)
-Left(Left_callback_1)
-Right(Right_callback_0)
-Right(Right_callback_1)
-# add 15ms delay to make sure events are registered correctly.
-wait(15, MSEC)
-
-ws2 = Thread( when_started2 )
-when_started1()
